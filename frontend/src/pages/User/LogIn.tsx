eb5dfbd0a788aabe3a66c5a3c9e3740fa0dce336
ejlieb <coryonronda@gmail.com>
2022-07-27 11:46:10 +0900

    User_CreateSignUp_mk1

--- frontend/src/pages/User/LogIn.tsx
+++ frontend/src/pages/User/LogIn.tsx
@@ -1,8 +1,55 @@
-export default function LogIn() {
+import { initialState } from "../../features/user/userSlice"
+import React from 'react'
+import { useFormik } from 'formik'
+import { request } from "../../utils/axios"
+import * as Yup from 'yup'
+import axios from "axios"
+import { useAppDispatch } from "../../app/hooks"
+import { saveToken } from "../../features/user/loginSlice"
+
+
+export default function Login() {
+  const dispatch = useAppDispatch()
+  const DOMAIN = "http://localhost:8080/"
+  const loginRequest: any = (method: string, url: string, data: object) => {
+    return axios({
+      url: DOMAIN + url,
+      method: 'post',
+      data: data
+    })
+      .then(res => {
+        const token = res.data.key
+        dispatch(saveToken(token))
+      })
+      .catch(err => {
+        console.error(err.response.data)
+      })
+  }
+
+  const formik = useFormik({
+    initialValues: {id: '', password:''},
+    validationSchema: Yup.object({
+      id: Yup.string()
+      .required('Required'),
+      password: Yup.string()
+      .required('Required'),
+    }),
+    onSubmit: (credentials) => {loginRequest('POST', 'api/user/login', credentials)} 
+  
+  })
     return (
-        <div>
-          <h1>LogIn</h1>
-        </div>
+      <div>
+      <h1>Login</h1>
+      <form action="" onSubmit={ formik.handleSubmit }>
+
+        <label htmlFor="id">Id</label>
+        <input id="id" name="id" type="text" onChange={formik.handleChange} value={ formik.values.id }/>
+
+        <label htmlFor="password">Password</label>
+        <input id="password" name="password" type="text" onChange={formik.handleChange} value={ formik.values.password }/>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
         
     )
 }
--- frontend/src/utils/axios.tsx
+++ frontend/src/utils/axios.tsx
@@ -1,6 +1,6 @@
-import axios from "axios";
+import axios from "axios"
 
-const DOMAIN = "http://localhost:8080/";
+const DOMAIN = "http://localhost:8080/"
 axios.defaults.withCredentials = true; // 쿠키 데이터를 전송받기 위해
 export const request : any = (method: any, url: any, data: any) => {
   console.log(data)
@@ -10,6 +10,20 @@ export const request : any = (method: any, url: any, data: any) => {
     data,
   })
     .then((res) => res.data)
-    .then((res) => console.log('hi'))
     .catch((err) => console.log(err));
 };
+
+// export const loginRequest: any = (method: string, url: string, data: object) => {
+//   return axios({
+//     url: DOMAIN + url,
+//     method: 'post',
+//     data: data
+//   })
+//     .then(res => {
+//       const token = res.data.key
+//       dispatch(saveToken(token))
+//     })
+//     .catch(err => {
+//       console.error(err.response.data)
+//     })
+// }
