aa4f2e634962ab55493bed752f7bc9c52a1fab6d
SSAFY <coryonronda@gmail.com>
2022-08-04 01:56:25 +0900

    User_fixLogin_mk3

--- frontend/src/features/user/loginSlice.tsx
+++ frontend/src/features/user/loginSlice.tsx
@@ -3,10 +3,10 @@ import axios from 'axios'
 import { useEffect } from 'react'
 import Cookies from 'universal-cookie'
 export type loginData = {
-    token: string
+    token: any
     credentials:object
     isLoggedIn: number
-    currentUser: string
+    currentUser: any
 }
 
 export type loginPayload ={
@@ -14,10 +14,10 @@ export type loginPayload ={
 }
 
 const initialState: loginData = {
-    token: '',
+    token: localStorage.getItem('token'),
     credentials: {},
     isLoggedIn: localStorage.getItem('token') === '' ? 0 : 1,
-    currentUser: '',
+    currentUser: localStorage.getItem('current_user'),
 
 }
 
@@ -30,6 +30,8 @@ const loginSlice = createSlice({
       saveToken: (state: loginData, action) => {
         state.token = action.payload.accessToken
         state.currentUser = action.payload.currentUser
+        localStorage.setItem('token', action.payload.accessToken)
+        localStorage.setItem('current_user', action.payload.currentUser)
         console.log(action.payload)
         cookies.set('refresh_token', action.payload.refreshToken, { sameSite: 'strict' });
 
@@ -37,6 +39,8 @@ const loginSlice = createSlice({
       removeToken: (state: loginData) => {
         state.token = ''
         state.currentUser = ''
+        localStorage.setItem('token', '')
+        localStorage.setItem('current_user', '')
         cookies.remove('refresh_token')
 
       },
