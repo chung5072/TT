38903e3597b986d8e7591bf38a26352730fb4f0d
이창현 <rover48@hanmail.net>
2022-08-12 16:55:58 +0900

    User_EDIT_DELETE_mk2

--- backend/src/main/java/com/tt9ood/api/controller/UserController.java
+++ backend/src/main/java/com/tt9ood/api/controller/UserController.java
@@ -69,6 +69,32 @@ public class UserController {
 		return ResponseEntity.status(200).body(userService.getUserByUserId(userId));
 	}
 
+	@PutMapping("/userinfo/{userId}")
+	@ApiOperation(value = "회원 본인 정보 수정", notes = "로그인한 회원 본인의 정보를 응답한다.")
+	@ApiResponses({
+			@ApiResponse(code = 200, message = "성공"),
+			@ApiResponse(code = 401, message = "인증 실패"),
+			@ApiResponse(code = 404, message = "사용자 없음"),
+			@ApiResponse(code = 500, message = "서버 오류")
+	})
+	public ResponseEntity<?> userUpdate(@PathVariable String userId, @RequestBody UserRegisterPostReq userDto) {
+		userService.update(userId, userDto);
+		return ResponseEntity.status(200).body(userId);
+	}
+
+	@DeleteMapping("/userinfo/{userId}")
+	@ApiOperation(value = "회원 본인 정보 삭제", notes = "로그인한 회원 본인의 정보를 응답한다.")
+	@ApiResponses({
+			@ApiResponse(code = 200, message = "성공"),
+			@ApiResponse(code = 401, message = "인증 실패"),
+			@ApiResponse(code = 404, message = "사용자 없음"),
+			@ApiResponse(code = 500, message = "서버 오류")
+	})
+	public ResponseEntity<?> userDelete(@PathVariable String userId) {
+		userService.delete(userId);
+		return ResponseEntity.status(200).body(userId);
+	}
+
 	@GetMapping("/naver/connect")
 	@ApiOperation(value = "네이버 인가코드 받기", notes = "네이버에서 인가코드를 받아온다")
 	@ApiResponses({
