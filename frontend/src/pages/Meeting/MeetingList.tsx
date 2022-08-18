bb7abc551d6220b138eb12a164272d587c7651fe
SSAFY <coryonronda@gmail.com>
2022-08-18 15:40:25 +0900

    Game_fixLogCss

--- frontend/src/pages/Meeting/MeetingList.tsx
+++ frontend/src/pages/Meeting/MeetingList.tsx
@@ -94,7 +94,7 @@ const MeetingList = () => {
               <tr>
                 <th scope="col" id='number'>NUMBER</th>
                 <th scope="col" id='title'>TITLE</th>
-                <th scope="col" id='name'>NAME</th>
+                <th scope="col" id='name'>NAME</th>z
               </tr>
             </thead>
             <tbody>
